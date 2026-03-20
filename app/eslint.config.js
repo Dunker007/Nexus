1c5707547b1c12aecdac81dc3f3fe9a711beded2
Rix-LuxRig <luxrig@dlx.local>
2026-03-20 14:51:23 -0500

    chore: add dist-server to eslint globalIgnores

--- app/eslint.config.js
+++ app/eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist', 'src-tauri']),
+  globalIgnores(['dist', 'dist-server', 'src-tauri']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
